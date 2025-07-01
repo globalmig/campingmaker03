26a5e2ef2aded46eea686925ddbb85b63d4147c5
user <kimji4545@gmail.com>
2025-07-01 17:03:29 +0900

    웹 마스터도구 등록

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ import Footer from "@/components/Footer";
 export const metadata: Metadata = {
   title: "캠핑메이커",
   description: "캠핑카 제작 홈페이지",
+  other: {
+    'naver-site-verification': '632bbdbe748e6125f42049c66acc306736a8ad8e',
+  }
 };
 
 export default function RootLayout({
@@ -17,9 +20,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-       <Header/>
-       {children}
-       <Footer/>
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
